708cebb2e92f757e60d3407d79bf55ccc6032d23
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-04-16 16:37:35 +0200

    Point to spatial-extension master branch

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -3,7 +3,7 @@
 echo "Pull the latest Spatial Extension changes"
 mkdir -p ./repos
 cd ./repos/carto-advanced-spatial-extension
-BRANCH=chore/ch147621/reference-documentation-synchronization # TODO: master
+BRANCH=master
 git checkout $BRANCH
 git pull --ff-only origin $BRANCH
 git submodule update --init
