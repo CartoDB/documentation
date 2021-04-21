10774804ef1cfe780007e34a0c773eebab607ef1
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-04-21 09:22:47 +0200

    Update spatial-extension repos in scripts

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -2,8 +2,12 @@
 
 echo "Pull the latest Spatial Extension changes"
 mkdir -p ./repos
-cd ./repos/carto-advanced-spatial-extension
+cd ./repos
+cd ./carto-spatial-extension
 BRANCH=master
 git checkout $BRANCH
 git pull --ff-only origin $BRANCH
-git submodule update --init
+cd ../carto-advanced-spatial-extension
+BRANCH=master
+git checkout $BRANCH
+git pull --ff-only origin $BRANCH
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 echo "Update Spatial Extension reference: bq"
-cd /repos/carto-advanced-spatial-extension
+cd /repos
+cd ./carto-spatial-extension
+sh /scripts/projects/spatial-extension.sh bq /src/content/spatial-extension-bq/sql-reference
+cd ../carto-advanced-spatial-extension
 sh /scripts/projects/spatial-extension.sh bq /src/content/spatial-extension-bq/sql-reference
