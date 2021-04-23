4b7cf53ac3ed681fa71822925b3b72780545ab1d
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-04-23 11:11:12 +0200

    Add SF reference

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,3 +6,10 @@ cd ./carto-spatial-extension
 sh /scripts/projects/spatial-extension.sh bq /src/content/spatial-extension-bq/sql-reference
 cd ../carto-advanced-spatial-extension
 sh /scripts/projects/spatial-extension.sh bq /src/content/spatial-extension-bq/sql-reference
+
+echo "Update Spatial Extension reference: sf"
+cd /repos
+cd ./carto-spatial-extension
+sh /scripts/projects/spatial-extension.sh sf /src/content/spatial-extension-sf/sql-reference
+cd ../carto-advanced-spatial-extension
+sh /scripts/projects/spatial-extension.sh sf /src/content/spatial-extension-sf/sql-reference
