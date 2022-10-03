392b5bc771038f486239f21e95e0856b7725ae65
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-10-03 09:15:17 +0200

    Point to AT master branch

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+BRACH=master
 CHECKOUT_DIR=./.checkout
 
 rm -rf $CHECKOUT_DIR
@@ -7,4 +8,4 @@ mkdir $CHECKOUT_DIR
 
 echo -e "\nCheckout Analytics Toolbox"
 echo "-------------------------------"
-git clone --branch feature/sc-254423/lds-warning-message-in-bq-rs-and-sf --depth 1 --recurse-submodules git@github.com:CartoDB/analytics-toolbox.git $CHECKOUT_DIR/analytics-toolbox
+git clone --branch $BRACH --depth 1 --recurse-submodules git@github.com:CartoDB/analytics-toolbox.git $CHECKOUT_DIR/analytics-toolbox
